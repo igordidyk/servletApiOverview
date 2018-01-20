db7076bc6a78841bcf225c42db80273e24ad3d26
Ihor Didyk <ihordidyk95@gmail.com>
2018-01-20 20:02:23 +0200

    added CrossSiteScriptingServlet.java class

--- src/main/java/com/tutorial/servlets/CrossSiteScriptingServlet.java
+++ src/main/java/com/tutorial/servlets/CrossSiteScriptingServlet.java
@@ -1,7 +1,40 @@
 package com.tutorial.servlets;
 
-/**
- * Created by Ihor Didyk on 20.01.2018.
- */
-public class CrossSiteScriptingServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/xss")
+public class CrossSiteScriptingServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String message = req.getParameter("message");
+        message = message == null ? "" : message.replaceAll("<", "&lt").replaceAll(">", "&gt");
+
+        resp.getWriter().write("<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Servlet Tutorial</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<p>\"message = " + message + "</p>" +
+                "<form action=\"/xss\" method=\"post\">\n" +
+                "    <textarea type=\"text\" name=\"message\"></textarea>" +
+                "    <input type=\"submit\" value=\"Send\">\n" +
+                "</form>\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
 }
