15f84d8e26bc9898bf07eb45e99c76b98f8735d3
Ihor Didyk <ihordidyk95@gmail.com>
2018-01-20 21:53:41 +0200

    added SessionServlet class

---
+++ src/main/java/com/tutorial/servlets/SessionServlet.java
@@ -0,0 +1,26 @@
+package com.tutorial.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet("/session")
+public class SessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Enumeration<String> attributeNames = session.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            String attributeName = attributeNames.nextElement();
+            System.out.println(attributeName + " = " + session.getAttribute(attributeName));
+        }
+        session.setAttribute("key","value");
+        System.out.println("MaxInactiveInterval = " + session.getMaxInactiveInterval());
+        session.removeAttribute("key");
+    }
+}
