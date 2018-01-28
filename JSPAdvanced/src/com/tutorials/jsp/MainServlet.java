d63b92575c1249f91d12c667d57a236415dd6e51
Ihor Didyk <ihordidyk95@gmail.com>
2018-01-28 15:47:23 +0200

    added changes to web.xml

--- JSPAdvanced/src/com/tutorials/jsp/MainServlet.java
+++ JSPAdvanced/src/com/tutorials/jsp/MainServlet.java
@@ -11,5 +11,6 @@ public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().write("hello :)");
+//        resp.sendRedirect("");
     }
 }
