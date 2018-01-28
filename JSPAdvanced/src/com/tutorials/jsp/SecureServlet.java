ab8fd458470fd3b2461ef7ed8e2334d634751746
Ihor Didyk <ihordidyk95@gmail.com>
2018-01-28 21:12:09 +0200

    configured https and SSH for security

--- JSPAdvanced/src/com/tutorials/jsp/SecureServlet.java
+++ JSPAdvanced/src/com/tutorials/jsp/SecureServlet.java
@@ -7,11 +7,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/logout")
+//@WebServlet("/logout")
 public class SecureServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getSession().invalidate();
+//        req.getSession().invalidate();
+        if (req.getAttribute("javax.servlet.request.key_size") == null) {
+            resp.sendRedirect("https://localhost:8443/admin");
+        }
+
+        if (req.isUserInRole("ADMIN")) {
+            System.out.println("Remote User = " + req.getRemoteUser());
+            System.out.println("Remote Username = " + req.getUserPrincipal().getName());
+        }
+
 
     }
 }
