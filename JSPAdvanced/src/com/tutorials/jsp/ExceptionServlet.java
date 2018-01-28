9857711fdd4933b9f9dc21260a79155053cc5048
Ihor Didyk <ihordidyk95@gmail.com>
2018-01-28 20:00:15 +0200

    added error page to web.xml

--- JSPAdvanced/src/com/tutorials/jsp/ExceptionServlet.java
+++ JSPAdvanced/src/com/tutorials/jsp/ExceptionServlet.java
@@ -1,18 +1,16 @@
 package com.tutorials.jsp;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-public class MainServlet extends HttpServlet {
+@WebServlet("/exception")
+public class ExceptionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("hello :)");
-//        resp.sendRedirect("/jsp/index.jsp");
-
-
+        throw new ServletException("this is servlet exception");
     }
 }
